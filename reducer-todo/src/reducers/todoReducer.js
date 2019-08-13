40e96ee3ff243577b6849376e9f58db3f03cc7c4
namistarr <namistarr@icloud.com>
2019-08-13 13:22:51 -0700

    reducer file created, basic reducer added, initial state object created

---
+++ reducer-todo/src/reducers/todoReducer.js
@@ -0,0 +1,44 @@
+export const todoData = [
+    {
+        task: 'Go over TK before lecture',
+        id: 1,
+        completed: false
+      },
+      {
+        task: 'Eat lunch',
+        id: 2,
+        completed: false
+      },
+      {
+        task: 'Work on todays project', 
+        id:3,
+        completed: false
+      },
+      {
+        task: 'Go first @ standup',
+        id: 4,
+        completed: false
+      },
+      {
+        task: 'Watch Buffy', 
+        id: 5,
+        completed: false
+    }
+];
+
+
+
+export const todoReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state, {
+                    id: Date.now(),
+                    task: action.payload,
+                    completed: false
+                }
+            ];            
+        default: 
+            return state;
+    }
+}
